7760e89b6819125fad4409da1c2acade6b3c5928
RobertWantling <robert6995@hotmail.co.uk>
2023-10-09 16:02:55 +0100

    create AJAX request to API for recipes

--- dist/index.62406edb.js
+++ dist/index.62406edb.js
@@ -6,5 +6,19 @@ const timeout = function(s) {
         });
     });
 };
+// make AJAX request to an API use fetch
+const showRecipe = async function() {
+    try {
+        const response = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        // fetch will return a promise, as its in async function can then await that promise
+        // convert to JSON - json method is available on all response objects. Response object is exactly what the fetch function here returns
+        const data = await response.json();
+        if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+        console.log(response, data);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
 
 //# sourceMappingURL=index.62406edb.js.map
